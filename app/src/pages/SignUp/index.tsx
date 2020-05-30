2d4c467d200ee3a28cb9dab9aded59e0d06afbfd
Otacilio Fox <otaciliomanoel.neto@gmail.commit>
2020-05-30 11:43:33 -0300

    create mobile app

---
+++ app/src/pages/SignUp/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Text } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Text>SignIn</Text>
+    </Container>
+  );
+};
+
+export default SignUp;
