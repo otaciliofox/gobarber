cea0821f0af02dc5be0e9d27ae1c331470bd81bb
Otacilio Fox <otaciliomanoel.neto@gmail.commit>
2020-05-30 12:03:20 -0300

    feature - create navigation signin and signup

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -6,6 +6,7 @@ import {
   KeyboardAvoidingView,
   Platform,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import Button from '../../components/Button';
@@ -22,6 +23,8 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <KeyboardAvoidingView
@@ -50,7 +53,7 @@ const SignIn: React.FC = () => {
           </Container>
         </ScrollView>
 
-        <CreateAccountButton onPress={() => {}}>
+        <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
           <Icon name="log-in" size={20} color="#ff9000" />
           <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
         </CreateAccountButton>
